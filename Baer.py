b187ee978103aaf4122c9d2d2ffbccc2fbc88509
AK <andrea.krueger@abs-it-kiel.de>
2022-07-26 14:26:13 +0200

    modified:   .gitignore 	modified:   Baer.py

--- Baer.py
+++ Baer.py
@@ -1,2 +1,19 @@
+import itertools
+
 class Baer:
-    pass
+    name = "e"
+    nummer = itertools.count()
+    alter = 0
+    gewicht = 0
+    groesse = 0
+    def __init__(self, name, alter, gewicht, groesse):
+        self.name = name
+        self.alter = alter
+        self.gewicht = gewicht
+        self.groesse = groesse
+        self.IDnummer = next(self.nummer)
+    def schlafen(self, dauer):
+        print(str(self.name) + " schläft " + str(dauer) + " Minuten.")
+    def fressen(self):
+        print(str(self.name) + " frisst.")
+
