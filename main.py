d66c3e305a194643863dcc62c1da85c3893ed53e
ElenaDieErste <elena.werschinin@abs-it-kiel.de>
2022-07-26 14:26:37 +0200

    ich hab alles kaput gemacht haha

---
+++ main.py
@@ -0,0 +1,15 @@
+from brown import Brown
+from Panda import Panda
+from EisBaer import EisBaer
+
+
+hippo = Brown(23,45,67)
+hippo.klettern()
+
+panda = Panda(11,23,34)
+panda.rollen()
+panda.kraulen()
+
+eis = EisBaer(66,55,77)
+eis.schwimmen()
+
