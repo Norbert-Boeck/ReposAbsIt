0b51f89ca6fe713590743a1bcd6fdb11efb396a2
ElenaDieErste <elena.werschinin@abs-it-kiel.de>
2022-07-26 15:09:35 +0200

    ich habe alles kaputt gemacht haha def kraulen in Panda erstellt

--- Baer.py
+++ Baer.py
@@ -1,2 +1,19 @@
+from socketserver import BaseRequestHandler
+
+
 class Baer:
-    pass
+    alter = 0 
+    gewicht = 0
+    groesse = 0
+    def schlafen(self,dauer):
+        print("Schlafe " + str(dauer) + " Minuten")
+    def fressen(self):
+        print("fressen")
+    def __init__(self, alter, gewicht, groesse):
+        self.alter = alter
+        self.gewicht = gewicht
+        self.groesse = groesse
+balu = Baer(30,650,200)
+bruno = Baer(10,450,500)
+balu = bruno
+
---
+++ Panda.py
@@ -0,0 +1,8 @@
+from Baer import Baer
+
+class Panda(Baer):
+    def rollen(self):
+        print("ich kann rollen")
+    def kraulen(self):
+        print("ich kann kraulen")
+    
