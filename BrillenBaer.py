e39b163f675f9e0da2e9c0323c0a12bad7cad187
Michael Steenbock <Michael.Steenbock@abs-it-kiel.de>
2022-07-26 15:13:19 +0200

    Alles Neu (MIchael)

--- BrillenBaer.py
+++ BrillenBaer.py
@@ -2,5 +2,7 @@ from Baer import Baer
 class BrillenBaer(Baer):
     def schnueffeln(self):
         print("der Brillenbär schnüffelt")
+        
+#alles folgende gehört in die Main.py
 klaus = BrillenBaer(12,120,120)
 klaus.schnueffeln()
