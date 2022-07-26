09aadabad85939d876c2507479432edca1d133ed
Norbert Boeck <norbert.boeck@abs-it-kiel.de>
2022-07-26 14:55:29 +0200

    Klasse Baer erstellt

---
+++ Baer.py
@@ -0,0 +1,13 @@
+class Baer:
+    alter = 0
+    gewicht = 0
+    groesse = 0
+
+    def schlafen(self,dauer):
+        print("Schlafe " + str(dauer) + " Minuten")
+    def fressen(self):
+        print("fressen")
+    def __init__(self,alter,gewicht,groesse):
+        self.alter=alter
+        self.gewicht=gewicht
+        self.groesse=groesse
